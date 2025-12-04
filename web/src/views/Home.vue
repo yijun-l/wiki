65897a5fe7c5b87f87fcd505bc60bf1ac135c2ff
Yijun <buglittlej@gmail.com>
2025-12-04 18:24:37 +0800

    feat(web): enable frontend-backend communication
    - Add Axios: npm install axios
    - Implement Spring Boot CORS configuration to allow cross-origin requests
    - Test API connectivity with /ebook/list endpoint

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
-        Content
-    </div>
+  <div>
+    <button @click="fetchData">Fetch Data (Console Log Only)</button>
+  </div>
 </template>
+
+<script setup>
+import axios from 'axios'
+
+const fetchData = async () => {
+  try {
+    const res = await axios.get('http://localhost:8080/ebook/list?id=3')
+    console.log('Data:', res.data)
+  } catch (err) {
+    console.error('Error:', err)
+  }
+}
+</script>
