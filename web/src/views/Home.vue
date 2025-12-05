ddac35f2d59fc461a3798164f6bc6acb03822f61
Yijun <buglittlej@gmail.com>
2025-12-05 12:29:55 +0800

    feat(web): add Ant Design Vue icon dependency
    - npm install --save @ant-design/icons-vue
    - Included test usage to verify icon components work correctly

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <button @click="fetchData">Fetch Data (Console Log Only)</button>
+    <br>
+    <star-outlined />
+    <star-filled />
+    <star-two-tone two-tone-color="#eb2f96" />
   </div>
 </template>
 
 <script setup>
-import axios from 'axios'
+import axios from 'axios';
+import { StarOutlined, StarFilled, StarTwoTone } from '@ant-design/icons-vue';
 
 const fetchData = async () => {
   try {
