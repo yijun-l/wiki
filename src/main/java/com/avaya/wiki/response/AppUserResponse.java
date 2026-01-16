dd1d45328834f8388b750ae5d8981cead2747772
Yijun <buglittlej@gmail.com>
2026-01-16 11:31:41 +0800

    fix(response): include `id` in `AppUserResponse`
    - Added `id` field to `AppUserResponse` DTO so front-end can reference entries
    - Updated MyBatis select query to include `id`

--- src/main/java/com/avaya/wiki/response/AppUserResponse.java
+++ src/main/java/com/avaya/wiki/response/AppUserResponse.java
@@ -1,11 +1,20 @@
 package com.avaya.wiki.response;
 
 public class AppUserResponse {
+    private Long id;
     private String username;
     private String email;
     private String nickname;
     private String avatarUrl;
     private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
