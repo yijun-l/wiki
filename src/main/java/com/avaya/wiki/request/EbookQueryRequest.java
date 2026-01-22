7f866d9813ca66853f71c270b11c17e7510f3757
Yijun <buglittlej@gmail.com>
2026-01-22 17:20:14 +0800

    feat(request): support category filtering in `list` API
    - Added `cat1Id` and `cat2Id` in `EbookQueryRequest`
    - Updated MyBatis XML to filter by `cat1Id` and `cat2Id`

--- src/main/java/com/avaya/wiki/request/EbookQueryRequest.java
+++ src/main/java/com/avaya/wiki/request/EbookQueryRequest.java
@@ -1,7 +1,9 @@
 package com.avaya.wiki.request;
 
-public class EbookQueryRequest extends PageQuery{
+public class EbookQueryRequest extends PageQuery {
     private String name;
+    private Long cat1Id;
+    private Long cat2Id;
 
     public String getName() {
         return name;
@@ -10,4 +12,22 @@ public class EbookQueryRequest extends PageQuery{
     public void setName(String name) {
         this.name = name;
     }
+
+    public Long getCat1Id() {
+        return cat1Id;
+    }
+
+    public void setCat1Id(Long cat1Id) {
+        this.cat1Id = cat1Id;
+    }
+
+    public Long getCat2Id() {
+        return cat2Id;
+    }
+
+    public void setCat2Id(Long cat2Id) {
+        this.cat2Id = cat2Id;
+    }
+
+
 }
